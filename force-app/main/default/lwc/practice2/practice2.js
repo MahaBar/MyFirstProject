cf8d38d8bdaa2850b04825a49957531aa747f313
MahaBar <mahabar0807@gmail.com>
2023-05-23 12:44:10 -0400

    Binding practice

---
+++ force-app/main/default/lwc/practice2/practice2.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+
+export default class Practice2 extends LightningElement {
+
+    firstName = "John"; 
+    lastName = "Snow"; 
+
+    // getter is a type of function 
+    // that return value
+    // usually used for getting calculated result
+    // from the fields instead of creating new fields
+    get fullName() {
+        //return this.firstName + ' ' + this.lastName; 
+        return `${this.firstName} ${this.lastName}`;
+    }
+    
+    // add a function| method called handle click 
+    // this method will get executed when the button is clicked
+    // since we have onclick attribute of the button point to this method name
+    handleClick() {
+        console.log('Button is Clicked');
+        this.firstName = 'Clark';
+        this.lastName = 'Kent'; 
+    }
+
+
+}
