c154dc6b8958c5c6cd479774f96665f4e16720a9
MahaBar <mahabar0807@gmail.com>
2023-06-09 09:50:38 -0400

    Day 65 Parent to child

--- force-app/main/default/lwc/practice30/practice30.js
+++ force-app/main/default/lwc/practice30/practice30.js
@@ -1,3 +1,10 @@
 import { LightningElement } from "lwc";
 export default class Practice30 extends LightningElement {
+
+    inputMsg ; 
+
+    handleChange(event) {
+        this.inputMsg = event.target.value; 
+    }
+
 }
